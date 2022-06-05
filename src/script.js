f7ae642a1d97d0cb7d35ee8a2cbdf413c307f78a
limagisele <limagisele05@gmail.com>
2022-06-05 14:54:24 +1000

    add hamburger menu

--- src/script.js
+++ src/script.js
@@ -1,4 +1,26 @@
 $(document).ready(function () {
+  const menu = document.querySelector(".menu");
+  const menuItems = document.querySelectorAll(".menuItem");
+  const hamburger = document.querySelector(".hamburger");
+  const closeIcon = document.querySelector(".closeIcon");
+  const menuIcon = document.querySelector(".menuIcon");
+
+  function toggleMenu() {
+    if (menu.classList.contains("showMenu")) {
+      menu.classList.remove("showMenu");
+      closeIcon.style.display = "none";
+      menuIcon.style.display = "block";
+    } else {
+      menu.classList.add("showMenu");
+      closeIcon.style.display = "block";
+      menuIcon.style.display = "none";
+    }
+  }
+
+  hamburger.addEventListener("click", toggleMenu);
+});
+
+$(document).ready(function () {
   $(".listing").click(function () {
     document.location = $(this).find("a").attr("href");
   });
@@ -6,13 +28,15 @@ $(document).ready(function () {
 
 $(document).ready(function () {
   $(".about").on("click", function () {
-      $(this).find(".text").toggleClass("show-text");
-      $(this).find("figure").toggleClass("no-filter");
+    $(this).find(".text").toggleClass("show-text");
+    $(this).find("figure").toggleClass("no-filter");
   });
 });
 
 $(document).ready(function () {
   $("[type='submit']").on("click", function () {
-    alert("This is an static form and message has not been received.\nWould you mind sending and email instead? 😊\nThanks! ");
+    alert(
+      "This is a static form and message has not been received.\nWould you mind sending and email instead? 😊\nThanks! "
+    );
   });
 });
