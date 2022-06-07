8ba68c4f1525571770ed81b0d3a5190bce9dd229
limagisele <limagisele05@gmail.com>
2022-06-07 11:56:51 +1000

    add defer to script src tag

--- src/script.js
+++ src/script.js
@@ -1,46 +1,42 @@
 // Hamburger toggle menu
-$(document).ready(function () {
-  const menu = document.querySelector(".menu");
-  const menuItems = document.querySelectorAll(".menuItem");
-  const hamburger = document.querySelector(".hamburger");
-  const closeIcon = document.querySelector(".closeIcon");
-  const menuIcon = document.querySelector(".menuIcon");
+const menu = document.querySelector(".menu")
+const hamburger = document.querySelector(".hamburger")
+const closeIcon = document.querySelector(".closeIcon")
+const menuIcon = document.querySelector(".menuIcon")
 
-  function toggleMenu() {
-    if (menu.classList.contains("showMenu")) {
-      menu.classList.remove("showMenu");
-      closeIcon.style.display = "none";
-      menuIcon.style.display = "block";
-    } else {
-      menu.classList.add("showMenu");
-      closeIcon.style.display = "block";
-      menuIcon.style.display = "none";
-    }
+function toggleMenu() {
+  menu.classList.toggle("showMenu")
+  if (menu.classList.contains("showMenu")) {
+    closeIcon.style.display = "block"
+    menuIcon.style.display = "none"
+  } else {
+    menuIcon.style.display = "block"
+    closeIcon.style.display = "none"
   }
+}
 
-  hamburger.addEventListener("click", toggleMenu);
-});
+hamburger.addEventListener("click", toggleMenu)
 
 // Make entire project cards clickable
 $(document).ready(function () {
   $(".listing").click(function () {
-    document.location = $(this).find("a").attr("href");
-  });
-});
+    document.location = $(this).find("a").attr("href")
+  })
+})
 
 // Make cards on about page interactive showing text and removing filter on click
 $(document).ready(function () {
   $(".about").on("click", function () {
-    $(this).find(".text").toggleClass("show-text");
-    $(this).find("figure").toggleClass("no-filter");
-  });
-});
+    $(this).find(".text").toggleClass("show-text")
+    $(this).find("figure").toggleClass("no-filter")
+  })
+})
 
 // Add alert message on static form from contact page
 $(document).ready(function () {
   $("[type='submit']").on("click", function () {
     alert(
       "This is a static form and message has not been received.\nWould you mind sending and email instead? 😊\nThanks! "
-    );
-  });
-});
+    )
+  })
+})
